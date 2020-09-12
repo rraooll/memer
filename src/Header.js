7d709af048a07fc79de7a3508a162d26a76703aa
Rahul <workwithrahul101@gmail.com>
2020-09-12 21:07:01 +0530

    This is my meme app

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import   './Header.css';
+
+
+function Header() {
+    return (
+        <div className="Header">
+             <img
+        src='http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png'
+        alt='Problem?'
+      />
+            <h1 className="heading">Random Meme Generator</h1>
+            <img
+        src='http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png'
+        alt='Problem?'
+      />
+        </div>
+    )
+}
+
+export default Header
